2fc2cc292aa8db64eb99d98fc2175f33a5df94f5
antonionicolaci <antonionicolaci48@gmail.com>
2023-07-18 18:41:14 +0200

    add use model / change relation

--- app/Models/Type.php
+++ app/Models/Type.php
@@ -2,8 +2,9 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Work;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Type extends Model
 {
@@ -13,6 +14,6 @@ class Type extends Model
 
     public function works()
     {
-        return $this->belongTo(Work::class);
+        return $this->hasMany(Work::class);
     }
 }
